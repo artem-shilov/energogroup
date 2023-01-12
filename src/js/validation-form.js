19aa07b503c7e10b310e30bef6b7b8f801786fff
Artem Shilov <artemshilov75@gmail.com>
2023-01-12 19:34:07 +0300

    some fix

--- src/js/validation-form.js
+++ src/js/validation-form.js
@@ -4,26 +4,31 @@ const formInputs = document.querySelectorAll('.input');
 const popupInputs = document.querySelectorAll('.popup-input');
 const popupCheckbox = document.querySelector('.checkbox-popup');
 const checkbox = document.querySelector('.checkbox');
+// const errorMessage = document.querySelectorAll('.error-message');
+// const response = document.querySelector('.response');
 
 form.addEventListener('submit', (e) => {
   const emptyInputs = Array.from(formInputs).filter((e) => e.value === '');
-
+  // function showResponse() {
+  //   response.classList.add('show');
+  // }
   formInputs.forEach(function (e) {
-    if (e.value === '') {
+    if (e.value === '' ) {
       e.parentNode.classList.add('error-input');
     } else {
       e.parentNode.classList.remove('error-input');
     }
   });
 
-  if (emptyInputs.length !== 0) {
-    e.preventDefault();
-  }
   if (!checkbox.checked) {
     checkbox.classList.add('error');
     e.preventDefault();
   } else {
     checkbox.classList.remove('error');
+  }
+
+  if (emptyInputs.length !== 0) {
+    e.preventDefault();
   }
 });
 
@@ -33,19 +38,21 @@ popupForm.addEventListener('submit', (e) => {
   popupInputs.forEach(function (e) {
     if (e.value === '') {
       e.parentNode.classList.add('error');
+    
     } else {
       e.parentNode.classList.remove('error');
+    
     }
   });
 
-  if (emptyInputs.length !== 0) {
-    e.preventDefault();
-  }
   if (!popupCheckbox.checked) {
     popupCheckbox.classList.add('error');
     e.preventDefault();
   } else {
     popupCheckbox.classList.remove('error');
+  }
+  if (emptyInputs.length !== 0) {
+    e.preventDefault();
   }
 });
 
